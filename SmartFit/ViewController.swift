205e0de8b8c170b1b60ec0ce76acae2f18e5e27b
John Law <lawchihon@ymail.com>
2017-04-12 19:08:19 -0700

    Renamed the app and implemented basic google sign in

---
+++ SmartFit/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SmartFit
+//
+//  Created by John Law on 12/4/2017.
+//  Copyright © 2017 CPF-17. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
